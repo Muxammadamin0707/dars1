861ecda5224f35b5c66dc954377085a27e40e53c
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-04-15 10:50:05 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,5 @@
+let ism = prompt("Ismindiz nima: ");
+let famila = prompt("familaz ni kriting");
+let Yoshingizni = prompt("Yoshingizni kriting: ");
+let qiziqish = prompt("qiziqish ni kriting");
+alert(ism + " " + famila + " " + Yoshingizni + " yosh " + " " + qiziqish + "!");
